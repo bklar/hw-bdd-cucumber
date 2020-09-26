81e7a9928eec05366b0484bea95e1674d7768b3a
EC2 Default User <ec2-user@ip-172-31-9-240.us-east-2.compute.internal>
2020-09-26 01:48:22 +0000

    Part 3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,22 +1,23 @@
 # Add a declarative step here for populating the DB with movies.
 
+#_____________________________PART 1_______________________________________
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     Movie.create!(movie) #Simply create a movie for every entry in the table.
   end
 end
+#__________________________________________________________________________
 
+
+#_____________________________PART 2_______________________________________
 Then /(.*) seed movies should exist/ do | n_seeds |
   Movie.count.should be n_seeds.to_i
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
-
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+Then /I should see all of the movies/ do
+  # Make sure that all the movies in the app are visible in the table
+  # page.body is the page's HTML body as one giant string.
+  expect(page.all('table#movies tbody tr').length).to eq Movie.count
 end
 
 #handles multiple checkboxes
@@ -31,12 +32,18 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
     end
   end
 end
+#__________________________________________________________________________
 
-Then /I should see all of the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  # page.body is the page's HTML body as one giant string.
-  expect(page.all('table#movies tbody tr').length).to eq Movie.count
+
+
+#_____________________________PART 3_______________________________________
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  r = /#[\s\S]*{e1}.[\s\S]*{e2}/m
+  expect r.match(page.body)
 end
+#__________________________________________________________________________
 
 #using from web_steps.rb
 #When /^(?:|I )press "([^"]*)"$/ do |button|
