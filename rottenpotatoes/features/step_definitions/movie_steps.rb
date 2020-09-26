be02cf91760578a5ee87a30e7bb82c1bd26b1e7d
EC2 Default User <ec2-user@ip-172-31-9-240.us-east-2.compute.internal>
2020-09-26 01:02:38 +0000

    Part 2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -19,18 +19,26 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   fail "Unimplemented"
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
+#handles multiple checkboxes
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  #split the line on the comma
+  rating_list.split(', ').each do |rating|
+    #uncheck is a Boolean, if true uncheck, false check
+    if uncheck
+      uncheck ("ratings_#{rating}")
+    else
+      check ("ratings_#{rating}")
+    end
+  end
 end
 
-Then /I should see all the movies/ do
+Then /I should see all of the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  # page.body is the page's HTML body as one giant string.
+  expect(page.all('table#movies tbody tr').length).to eq Movie.count
 end
+
+#using from web_steps.rb
+#When /^(?:|I )press "([^"]*)"$/ do |button|
+  #click_button(button)
+#end
